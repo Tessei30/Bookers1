e0d21a227d3a4f23be84ffa0a17f2ae47d333d6a
tessei <tessei.29965560@gmail.com>
2020-08-16 19:55:46 +0900

    fixed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,23 @@
 class BooksController < ApplicationController
+
+def top
+  
+end
+
   def index
     @books = Book.all
+    @book = Book.new
   end  
 
-  def new
-    @books = Book.all
-    @book = Book.new
-  end
+  
 
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(book.id), notice: "Book was successfully created."
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
     else
       @books = Book.all
-      render :new
+      render :index
     end
     
   end
@@ -28,10 +31,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
-    flash[:notice] = "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      render :edit
+    end
+    
   end
 
   def destroy
